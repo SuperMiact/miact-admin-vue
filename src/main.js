daf0f0b28314197adf11c4ffb3f62164c2297522
SuperMiact <506479916@qq.com>
2022-06-14 13:38:48 +0800

    token放入请求头

--- src/main.js
+++ src/main.js
@@ -17,6 +17,20 @@ Vue.config.productionTip = false
 
 Vue.use(Fragment.Plugin)
 
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // 判断是否存在token,如果存在将每个页面header添加token
+  if (sessionStorage.getItem("token")) {
+    // config.headers.common['Authorization'] = sessionStorage.getItem("token");
+    config.headers.common['token'] = sessionStorage.getItem("token");
+  }
+  return config
+}, function (error) {
+  router.push('/login')
+  return Promise.reject(error)
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
