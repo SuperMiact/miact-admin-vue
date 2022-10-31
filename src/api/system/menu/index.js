ecb8c2bf63ebc8143cd81ed6e853016411361626
SuperMiact <506479916@qq.com>
2022-10-31 17:14:55 +0800

    菜单图标使用json文件读取

--- src/api/system/menu/index.js
+++ src/api/system/menu/index.js
@@ -2,37 +2,39 @@ import request from '@/utils/request'
 
 // 获取菜单
 // export const getMenu = () => request.get('/mainMenu/getMenu')
-export function getMenu(data){
-    return request({
-        url:'/mainMenu/getMenu',
-        method:'get',
-        params:data
-    })
+export function getMenu (data) {
+  return request({
+    url: '/mainMenu/getMenu',
+    method: 'get',
+    params: data
+  })
 }
 
 // 添加菜单
-export function addMenu(data){
-    return request({
-        url:'/mainMenu/addMenu',
-        method:'post',
-        data:data
-    })
+export function addMenu (data) {
+  return request({
+    url: '/mainMenu/addMenu',
+    method: 'post',
+    data: data
+  })
 }
 
 // 修改菜单
-export function updateMenu(data){
-    return request({
-        url:'/mainMenu/updateMenu',
-        method:'post',
-        data:data
-    })
+export function updateMenu (data) {
+  return request({
+    url: '/mainMenu/updateMenu',
+    method: 'post',
+    data: data
+  })
 }
 
 // 删除菜单
-export function delMenu(params){
-    return request({
-        url:'/mainMenu/delMenu',
-        method:'get',
-        params:params
-    })
+export function delMenu (params) {
+  return request({
+    url: '/mainMenu/delMenu',
+    method: 'get',
+    params: params
+  })
 }
+
+export const getIcons = () => request.get(window.location.origin + '/static/iconData.json')
