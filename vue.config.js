c002dc754c8ba5374c41af52f3f26934ae0cc5bc
SuperMiact <506479916@qq.com>
2022-11-10 11:29:17 +0800

    修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,9 @@ function resolve(dir) {
 
 module.exports = {
     lintOnSave: true,
+    devServer: {
+      host: 'localhost',
+    },
     chainWebpack: (config) => {
       config.resolve.alias
         .set('@', resolve('src'))
