fbfba9e43604a5db2b20eefc03cceb5af3a9753d
SuperMiact <506479916@qq.com>
2023-08-31 00:15:21 +0800

    分配角色选择框 样式修改

--- src/views/system/user/bindRoles.vue
+++ src/views/system/user/bindRoles.vue
@@ -1,71 +1,74 @@
 <template>
-    <el-dialog ref="roleDig" title="选择角色" :visible.sync="showRoleModel" width="20%" center>
-      <el-form label-position="right" label-width="70px" :model="roleData" style="margin: 20px">
-        <div align="left">
-          <el-form-item label="角色类型">
-            <el-select v-model="roleData.roleId">
-              <el-option
-                v-for="item in roleList"
-                :key="item.roleId"
-                :label="item.roleName"
-                :value="item.roleId"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <div slot="footer" class="dialog-role" style="margin-top: 50px">
-            <el-button @click="closeRoleForm">取 消</el-button>
-            <el-button type="primary" style="margin-left: 50px" @click="submitRoleForm">确 定</el-button>
-          </div>
-        </div>
-      </el-form>
-    </el-dialog>
+  <el-dialog
+    ref="roleDig"
+    title="选择角色"
+    :visible.sync="showRoleModel"
+    width="380px"
+  >
+    <el-form label-position="right" label-width="70px" :model="roleData">
+      <el-form-item label="角色类型">
+        <el-select v-model="roleData.roleId">
+          <el-option
+            v-for="item in roleList"
+            :key="item.roleId"
+            :label="item.roleName"
+            :value="item.roleId"
+          ></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item align="right">
+        <el-button type="primary" @click="submitRoleForm">确 定</el-button>
+        <el-button @click="closeRoleForm">取 消</el-button>
+      </el-form-item>
+    </el-form>
+  </el-dialog>
 </template>
 <script>
 import { queryRoles } from "@/api/system/role";
-import {bindRole} from "@/api/system/user";
+import { bindRole } from "@/api/system/user";
 export default {
-    name: "bindRoles",
-    components: {},
-    data(){
-        return{
-            type:'',
-            showRoleModel:false,
-            roleList:[],
-            roleData:{},
+  name: "bindRoles",
+  components: {},
+  data() {
+    return {
+      type: "",
+      showRoleModel: false,
+      roleList: [],
+      roleData: {},
+    };
+  },
+  created() {
+    this.getRoleData();
+  },
+  methods: {
+    show(type, data) {
+      this.type = type;
+      this.roleData = data;
+      this.showRoleModel = true;
+    },
+    getRoleData() {
+      queryRoles({ pageNo: 1, pageSize: 100 }).then((res) => {
+        if (res.code === "0000") {
+          this.roleList = res.results.data;
         }
+      });
     },
-    created(){
-        this.getRoleData()
+    submitRoleForm() {
+      let roleData = this.roleData;
+      bindRole(roleData.id, roleData.roleId).then((res) => {
+        if (res.code == "0000") {
+          this.$emit("submitRole", true, res.message);
+        } else {
+          this.$message.error(res.message);
+        }
+      });
+      this.closeRoleForm();
     },
-    methods: {
-        show(type,data){
-            this.type = type
-            this.roleData = data
-            this.showRoleModel = true
-        },
-        getRoleData(){
-            queryRoles({pageNo:1,pageSize:100}).then((res) => {
-                if (res.code === "0000"){
-                    this.roleList = res.results.data
-                }
-            });  
-        },
-        submitRoleForm(){
-            let roleData = this.roleData
-            bindRole(roleData.id, roleData.roleId).then((res) => {
-                if(res.code == '0000') {
-                  this.$emit('submitRole',true,res.message)
-                }else{
-                  this.$message.error(res.message)
-                }
-            })
-            this.closeRoleForm()
-        },
-        closeRoleForm(){
-            this.type = ''
-            this.roleData = {}
-            this.showRoleModel = false
-        },
-    }
-}
+    closeRoleForm() {
+      this.type = "";
+      this.roleData = {};
+      this.showRoleModel = false;
+    },
+  },
+};
 </script>
